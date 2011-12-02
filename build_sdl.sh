68925a41f90a5bd90eed19dccb36011ddf7992e0
Mathieu Virbel <mat@kivy.org>
2011-12-02 16:28:03 +0100

    update build sdl to fix issue with divmodsi4 (use llvm-gcc-4.2 instead of clang, to keep the same compiler for all other projects.) + fix copy of libSDL.a into our build directory

--- build_sdl.sh
+++ build_sdl.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-. ./environmnent.sh
+. ./environment.sh
 pushd sdl/sdl1.3/Xcode-iPhoneOS/SDL
-xcodebuild -project SDLiPhoneOS.xcodeproj -alltargets
+xcodebuild -project SDLiPhoneOS.xcodeproj -target libSDL
 popd
+
 #the binarie is under sdl/sdl1.3/Xcode-iPhoneOS/SDL/build/Release-Universal/
 cp sdl/sdl1.3/Xcode-iPhoneOS/SDL/build/Release-iPhoneOS/libSDL.a $BUILDROOT/lib
 cp -a sdl/sdl1.3/include $BUILDROOT/include/SDL 
