6bcb741ebbfc5880af289c6be28f22ccaf3c9280
Mathieu Virbel <mat@kivy.org>
2011-12-06 15:33:48 +0100

    fix reduce_python when used in standalone mode

--- reduce_python.sh
+++ reduce_python.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+. environment.sh
+
 echo "Starting reducing=========="
 
-pushd $BUILDROOT/python/lib/python2.7
+try pushd $BUILDROOT/python/lib/python2.7
 find . -iname '*.pyc' | xargs rm
 find . -iname '*.py' | xargs rm
 find . -iname 'test_*' | xargs rm -rf
 rm -rf *test* config lib* wsgiref bsddb curses idlelib hotshot || true
-popd
+try popd
 rm -rf pkgconfig || true
