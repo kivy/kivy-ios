7d7cd44874aac230b245642b9deaf18cac3d602a
Mathieu Virbel <mat@kivy.org>
2012-05-25 01:23:21 +0200

    fix build script for latest sdl

--- tools/build-sdl.sh
+++ tools/build-sdl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . $(dirname $0)/environment.sh
-pushd $KIVYIOSROOT/src/SDL/Xcode-iPhoneOS/SDL
-xcodebuild -project SDLiPhoneOS.xcodeproj -target libSDL -configuration Release -sdk iphoneos$SDKVER
+pushd $KIVYIOSROOT/src/SDL/Xcode-iOS/SDL
+xcodebuild -project SDL.xcodeproj -target libSDL -configuration Release -sdk iphoneos$SDKVER
 popd
 
-cp src/SDL/Xcode-iPhoneOS/SDL/build/Release-iPhoneOS/libSDL.a $BUILDROOT/lib
+cp src/SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL.a $BUILDROOT/lib
 cp -a src/SDL/include $BUILDROOT/include/SDL 
