7f5a10a6bfa55fed9f97c3ab4d4c2340b3e86c64
Mathieu Virbel <mat@kivy.org>
2012-05-25 18:35:22 +0200

    remove SDL build directory

--- src/SDL/Xcode-iOS/SDL/build/SDL.build/Release-iphoneos/libSDL.build/Objects-normal/armv7/jumphack.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/tito/code/ios/kivy-ios/src/SDL/Xcode-iOS/SDL/../../src/video/uikit/jumphack.c \
-  /Users/tito/code/ios/kivy-ios/src/SDL/Xcode-iOS/SDL/../../src/video/uikit/jumphack.h
