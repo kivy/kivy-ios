8d8e26c75f8187e937d6e6d7f90aff9b3b26b28e
Mathieu Virbel <mat@kivy.org>
2012-12-09 21:21:00 +0100

    change kivy default branch to master.

--- tools/build-kivy.sh
+++ tools/build-kivy.sh
@@ -6,15 +6,13 @@ if [ ! -d $TMPROOT/kivy ] ; then
 	try pushd $TMPROOT
 	try git clone https://github.com/kivy/kivy
 	try cd kivy
-	try git checkout ios-support
 	try popd
 fi
 
 if [ "X$1" = "X-f" ] ; then
 	try pushd $TMPROOT/kivy
 	try git clean -dxf
-	try git fetch
-	try git checkout ios-support
+	try git pull origin master
 	try popd
 fi
 
