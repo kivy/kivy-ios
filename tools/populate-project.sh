3d6297e6fc4978cd3aaab9555610dda124397f9e
Mathieu Virbel <mat@kivy.org>
2013-01-23 02:02:40 +0100

    use rsync to populate the project instead of rm + cp -a

--- tools/populate-project.sh
+++ tools/populate-project.sh
@@ -11,6 +11,7 @@ APPID=$(echo $APPNAME | tr '[A-Z]' '[a-z]')
 APPDIR=$KIVYIOSROOT/app-$APPID
 SRCDIR=$2
 
+set -x
 if [ "X$APPNAME" == "X" ]; then
 	echo $(basename $0) "<appname> <source directory>"
 	exit 1
@@ -24,13 +25,8 @@ fi
 echo "-> Copy $SRCDIR to $APPDIR/YourApp"
 YOURAPPDIR=$APPDIR/YourApp
 
-echo "-> Remove any previous YourApp version"
-if [ -e $YOURAPPDIR ]; then
-	rm -r $YOURAPPDIR
-fi
-
-echo "-> Copy the new source"
-try cp -a $SRCDIR $YOURAPPDIR
+echo "-> Synchronize source code"
+try rsync -av --delete $SRCDIR $YOURAPPDIR
 
 echo "-> Compile to pyo"
 $TMPROOT/Python-$PYTHON_VERSION/hostpython -OO -m compileall $YOURAPPDIR
