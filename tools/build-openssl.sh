d11db8854e18ca667df76ae2831b44965b87bfa3
Richard Larkin <richard@camiweb.com>
2014-05-05 21:40:05 +0200

    Corrected file check

--- tools/build-openssl.sh
+++ tools/build-openssl.sh
@@ -18,19 +18,30 @@ fi
 echo "About to test ..."
 if [ -d $TMPROOT/openssl/ios-openssl ] ; then
     echo "ios-openssl folder found. Looking for binary..."
-    if [ ! $TMPROOT/openssl/ios-openssl/lib/libssl.a ] ; then
+    if [ ! -e $TMPROOT/openssl/ios-openssl/lib/libssl.a ] ; then
         echo "Binary not found. Building..."
-        try mkdir $TMPROOT/openssl/ios-openssl/lib
+        if [ ! -d $TMPROOT/openssl/ios-openssl/lib ] ; then
+            try mkdir $TMPROOT/openssl/ios-openssl/lib
+        fi
         try pushd .
         cd $TMPROOT/openssl/ios-openssl
         # Please refer to the script below for details of the OpenSSL build
         sh build.sh
         try popd
+    else
+        echo "Binaries found. Skipping build..."
     fi
+else
+    echo "Folder found. Pulling latest..."
+    try pushd .
+    cd $TMPROOT/openssl/ios-openssl
+    git pull
+    try popd 
 fi
 
 # Copy the binaries to the appropriate places.
 if [ $TMPROOT/openssl/ios-openssl/lib/libssl.a ] ; then
+    echo "Copying built OpenSSL binaries..."
     cp $TMPROOT/openssl/ios-openssl/lib/libssl.a $BUILDROOT/lib/libssl.a
     cp $TMPROOT/openssl/ios-openssl/lib/libcrypto.a $BUILDROOT/lib/libcrypto.a
 fi
