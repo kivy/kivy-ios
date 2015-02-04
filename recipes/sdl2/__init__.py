b8145e02076e80167f4ae444163791259b158650
Mathieu Virbel <mat@kivy.org>
2015-02-04 16:23:15 +0100

    sdl2: fix version recipe

--- recipes/sdl2/__init__.py
+++ recipes/sdl2/__init__.py
@@ -7,7 +7,7 @@ import shutil
 
 class LibSDL2Recipe(Recipe):
     version = "2.0.3"
-    url = "https://www.libsdl.org/release/SDL2-2.0.3.tar.gz"
+    url = "https://www.libsdl.org/release/SDL2-{version}.tar.gz"
     library = "Xcode-iOS/SDL/build/Release-{arch.sdk}/libSDL2.a"
 
     def build_arch(self, arch):
