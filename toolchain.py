f22b2a7922869aead90350bad29ae734447f13c3
Sylvain Alborini <sylvain.alborini@isatis-prod.fr>
2015-02-10 12:29:03 +0100

    copy-paste bug fix

--- toolchain.py
+++ toolchain.py
@@ -194,7 +194,7 @@ class Context(object):
 
         # get the latest iphonesimulator version
         iphonesim = [x for x in sdks if "iphonesimulator" in x]
-        if not iphoneos:
+        if not iphonesim:
             ok = False
             print("Error: No iphonesimulator SDK installed")
         else:
