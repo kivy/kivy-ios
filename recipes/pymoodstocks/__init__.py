12c9b009a31890a3a9d01f38743561e1b86308d2
Mathieu Virbel <mat@kivy.org>
2015-02-27 19:57:47 +0100

    pymoodstocks: fix url

--- recipes/pymoodstocks/__init__.py
+++ recipes/pymoodstocks/__init__.py
@@ -3,7 +3,7 @@ from toolchain import PythonRecipe
 
 class PyMoodstocksRecipe(PythonRecipe):
     version = "master"
-    url = "https://github.com/tito/pymoodstock/archive/{version}.zip"
+    url = "https://github.com/tito/pymoodstocks/archive/{version}.zip"
     depends = ["moodstocks", "kivy", "pyobjus"]
     sources = ["src/ios"]
     archs = ["i386"]
