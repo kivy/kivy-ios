e05c8dab0f9cc48dd575fcd616e47ec08c570bf7
Mathieu Virbel <mat@kivy.org>
2015-03-04 16:42:32 +0100

    audiostream: add missing sdl2_mixer dependency

--- recipes/audiostream/__init__.py
+++ recipes/audiostream/__init__.py
@@ -5,7 +5,7 @@ class AudiostreamRecipe(CythonRecipe):
     version = "master"
     url = "https://github.com/kivy/audiostream/archive/{version}.zip"
     library = "libaudiostream.a"
-    depends = ["python", "sdl2"]
+    depends = ["python", "sdl2", "sdl2_mixer"]
     pre_build_ext = True
 
 
