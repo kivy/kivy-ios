3375a521954b5d8ff9a3bb2c08cedf6fb3d02e47
Robert Niederreiter <office@squarewave.at>
2016-10-14 10:05:44 +0200

    host_setuptools WIP

--- recipes/host_setuptools/__init__.py
+++ recipes/host_setuptools/__init__.py
@@ -9,15 +9,16 @@ import shutil
 class HostSetuptools(Recipe):
     depends = ["hostpython"]
     archs = ["x86_64"]
-    url = "ez_setup"
+    url = ""
 
     def prebuild_arch(self, arch):
         hostpython = sh.Command(self.ctx.hostpython)
         sh.curl("-O",  "https://bootstrap.pypa.io/ez_setup.py")
-        dest_dir = join(self.ctx.dist_dir, "root", "python")
-        build_env = arch.get_env()
-        build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python2.7', 'site-packages')
+        #dest_dir = join(self.ctx.dist_dir, "root", "python")
+        #build_env = arch.get_env()
+        #build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python2.7', 'site-packages')
         # shprint(hostpython, "./ez_setup.py", "--to-dir", dest_dir)
-        shprint(hostpython, "./ez_setup.py", _env=build_env)
+        #shprint(hostpython, "./ez_setup.py", _env=build_env)
+        shprint(hostpython, "./ez_setup.py")
 
 recipe = HostSetuptools()
