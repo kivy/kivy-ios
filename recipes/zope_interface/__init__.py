02e9e08b5af820c3029f2abc9cd7b7e056a4a999
Robert Niederreiter <office@squarewave.at>
2016-10-14 12:39:39 +0200

    ...

--- recipes/zope_interface/__init__.py
+++ recipes/zope_interface/__init__.py
@@ -1,39 +1,22 @@
-from toolchain import PythonRecipe, shprint
+from toolchain import CythonRecipe, shprint
 from os.path import join
-import sh, os
+import sh
 
-class ZopeInterfaceRecipe(PythonRecipe):
+
+class ZopeInterfaceRecipe(CythonRecipe):
     version = "4.3.2"
-    url="https://github.com/zopefoundation/zope.interface/archive/{version}.zip"
+    url = "https://github.com/zopefoundation/zope.interface/archive/{version}.zip"
+    #library = "libzopeinterface.a"
     depends = ["python", "host_setuptools"]
-    include_per_arch = True
+    cythonize = False
 
-    def get_environ(self, arch):
-        build_env = arch.get_env()
-        build_env["IOSROOT"] = self.ctx.root_dir
-        build_env["IOSSDKROOT"] = arch.sysroot
-        build_env["LDSHARED"] = join(self.ctx.root_dir, "tools", "liblink")
-        build_env["ARM_LD"] = build_env["LD"]
-        build_env["ARCH"] = arch.arch
-        return build_env
+    def get_recipe_env(self, arch):
+        env = super(ZopeInterfaceRecipe, self).get_recipe_env(arch)
+        #env["CC"] = "{} {}".format(env["CC"], env["CFLAGS"])
+        return env
 
     def build_arch(self, arch):
-        build_env = self.get_environ(arch)
-        hostpython = sh.Command(self.ctx.hostpython)
-        shprint(hostpython, "setup.py", "build_ext", _env=build_env)
+        super(ZopeInterfaceRecipe, self).build_arch(arch)
 
-    def install(self):
-        arch = list(self.filtered_archs)[0]
-        build_dir = self.get_build_dir(arch.arch)
-        os.chdir(build_dir)
-        hostpython = sh.Command(self.ctx.hostpython)
-        build_env = self.get_environ(arch)
-        del build_env["ARCH"]
-        dest_dir = os.path.join(self.ctx.dist_dir, "root", "python")
-        build_env['PYTHONPATH'] = os.path.join(
-            dest_dir, 'lib', 'python2.7', 'site-packages')
-        shprint(
-            hostpython, "setup.py", "install", "--prefix", dest_dir,
-            _env=build_env)
 
 recipe = ZopeInterfaceRecipe()
