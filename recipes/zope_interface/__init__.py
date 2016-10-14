bbd1dadb164554ac85b6a7746aeb8c33a2fd51f5
Robert Niederreiter <rnix@squarewave.at>
2016-10-14 13:04:01 +0200

    reduce zope_interface recipe

--- recipes/zope_interface/__init__.py
+++ recipes/zope_interface/__init__.py
@@ -1,22 +1,12 @@
-from toolchain import CythonRecipe, shprint
-from os.path import join
-import sh
+from toolchain import CythonRecipe
 
 
 class ZopeInterfaceRecipe(CythonRecipe):
+    name = "zope"
     version = "4.3.2"
     url = "https://github.com/zopefoundation/zope.interface/archive/{version}.zip"
-    #library = "libzopeinterface.a"
     depends = ["python", "host_setuptools"]
     cythonize = False
-
-    def get_recipe_env(self, arch):
-        env = super(ZopeInterfaceRecipe, self).get_recipe_env(arch)
-        #env["CC"] = "{} {}".format(env["CC"], env["CFLAGS"])
-        return env
-
-    def build_arch(self, arch):
-        super(ZopeInterfaceRecipe, self).build_arch(arch)
 
 
 recipe = ZopeInterfaceRecipe()
