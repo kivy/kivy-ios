5a8b47ec19466dcec2c2b12f570ac57151a08c9d
Mathieu Virbel <mat@kivy.org>
2018-10-30 14:59:29 +0100

    update to SDL2 2.0.8, dont use the special branch anymore. Tested on iPAD with iOS 9

--- recipes/sdl2/__init__.py
+++ recipes/sdl2/__init__.py
@@ -3,10 +3,10 @@ import sh
 
 
 class LibSDL2Recipe(Recipe):
-    #version = "2.0.3"
-    #url = "https://www.libsdl.org/release/SDL2-{version}.tar.gz"
-    version = "iOS-improvements"
-    url = "https://bitbucket.org/slime73/sdl-experiments/get/{version}.tar.gz"
+    version = "2.0.8"
+    url = "https://www.libsdl.org/release/SDL2-{version}.tar.gz"
+    #version = "iOS-improvements"
+    #url = "https://bitbucket.org/slime73/sdl-experiments/get/{version}.tar.gz"
     library = "Xcode-iOS/SDL/build/Release-{arch.sdk}/libSDL2.a"
     include_dir = "include"
     pbx_frameworks = ["OpenGLES", "AudioToolbox", "QuartzCore", "CoreGraphics",
@@ -20,15 +20,16 @@ class LibSDL2Recipe(Recipe):
 
     def build_arch(self, arch):
         env = arch.get_env()
+        quiet = ["-quiet"] if self.ctx.quiet else []
         shprint(sh.xcodebuild, self.ctx.concurrent_xcodebuild,
                 "ONLY_ACTIVE_ARCH=NO",
                 "ARCHS={}".format(arch.arch),
                 "CC={}".format(env['CC']),
                 "-sdk", arch.sdk,
                 "-project", "Xcode-iOS/SDL/SDL.xcodeproj",
-                "-target", "libSDL",
-                "-configuration", "Release")
+                "-target", "libSDL-iOS",
+                "-configuration", "Release",
+                *quiet)
 
 
 recipe = LibSDL2Recipe()
-
