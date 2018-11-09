0e52dab46872a2b7f3992ca4a3e65294759a331d
Mathieu Virbel <mat@kivy.org>
2018-11-09 19:14:05 +0100

    fix recipes order with hostpython/python alias that didnt work in the first run

--- recipes/hostpython.py
+++ recipes/hostpython.py
@@ -7,9 +7,15 @@ class HostpythonAliasRecipe(Recipe):
     def init_after_import(self, ctx):
         hostpython = ctx.state.get("hostpython")
         if not hostpython:
-            print("WARNING: no hostpython set yet, so you need to specify")
-            print("WARNING: either hostpython2 or hostpython3 in your deps")
-        else:
+            # search in wanted_recipes if it's the first time
+            if "hostpython2" in ctx.wanted_recipes:
+                hostpython = "hostpython2"
+            elif "hostpython3" in ctx.wanted_recipes:
+                hostpython = "hostpython3"
+            else:
+                print("WARNING: no hostpython set yet, so you need to specify")
+                print("WARNING: either hostpython2 or hostpython3 in your deps")
+        if python:
             self.depends = [hostpython]
 
 recipe = HostpythonAliasRecipe()
