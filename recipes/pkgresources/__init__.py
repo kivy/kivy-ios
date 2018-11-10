c9ca2de91240344c18fbc41aafa667225a017c69
Mathieu Virbel <mat@kivy.org>
2018-11-10 10:14:39 +0100

    fixes plyer and pkgresources recipes that was depending on old platform

--- recipes/pkgresources/__init__.py
+++ recipes/pkgresources/__init__.py
@@ -1,17 +1,16 @@
 from toolchain import Recipe, shprint
 from os.path import join
 import sh
-import os
 
 
 class pkg_resources(Recipe):
     depends = ["hostpython", "python"]
-    archs = ['i386']
+    archs = ["x86_64"]
     url = ""
 
     def prebuild_arch(self, arch):
-        sh.cp("pkg_resources.py", join(self.ctx.dist_dir, "root", "python", "lib", "python2.7", "site-packages", "pkg_resources.py"))
+        sh.cp("pkg_resources.py",
+            join(self.ctx.site_packages_dir, "pkg_resources.py"))
+
 
 recipe = pkg_resources()
-
-
--- recipes/plyer/__init__.py
+++ recipes/plyer/__init__.py
@@ -1,12 +1,10 @@
-# pure-python package, this can be removed when we'll support any python package
 from toolchain import PythonRecipe
-
 
 class PlyerRecipe(PythonRecipe):
     version = "master"
     url = "https://github.com/kivy/plyer/archive/{version}.zip"
     depends = ["python", "pyobjus"]
-    archs = ["i386"]
+    archs = ["x86_64"]
+
 
 recipe = PlyerRecipe()
-
