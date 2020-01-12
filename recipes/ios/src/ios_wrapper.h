558f987d9d518fc2394c0126963158a9985f80c2
Mirko <me@mirkogalimberti.com>
2020-01-12 12:21:16 +0100

    Moved some utils to ios_utils, webview accepts x,y coords, improved get_scale and get_dpi (#415)

--- recipes/ios/src/ios_wrapper.h
+++ recipes/ios/src/ios_wrapper.h
@@ -12,7 +12,7 @@ float ios_uiscreen_get_scale(void);
 int ios_uiscreen_get_dpi(void);
 padding ios_get_safe_area(void);
 void ios_open_url(char *url);
-void load_url_webview(char *url, int width, int height);
+void load_url_webview(char *url, int x, int y, int width, int height);
 
 typedef void (*ios_send_email_cb)(char *, void *);
 
