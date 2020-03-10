a583d5db0789cede978889ec31cdeb672a975bdb
Richard Larkin <zenkey.zencode@gmail.com>
2020-03-10 21:24:31 +0200

    Add NativeImagePicker class for use from the player IOS FileChooser class (#428)
    Co-authored-by: richard <richard@dotmodus>

--- recipes/ios/src/setup.py
+++ recipes/ios/src/setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup, Extension
 import os
 
 setup(name='ios',
-      version='1.0',
+      version='1.1',
       ext_modules=[
         Extension(
-            'ios', ['ios.c', 'ios_utils.m', 'ios_mail.m', 'ios_browser.m'],
+            'ios', ['ios.c', 'ios_utils.m', 'ios_mail.m', 'ios_browser.m',
+                    'ios_filechooser.m'],
             libraries=[ ],
             library_dirs=[],
             )
