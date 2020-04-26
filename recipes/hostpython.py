35d16cd5325b5b494a00d45d2c08b1a8968c80cd
richard <richard@dotmodus>
2020-04-26 09:18:20 +0200

    Removed python2.7 recipes for python and hostpython

--- recipes/hostpython.py
+++ recipes/hostpython.py
@@ -10,20 +10,10 @@ class HostpythonAliasRecipe(Recipe):
     is_alias = True
 
     def init_after_import(self, ctx):
-        hostpython = ctx.state.get("hostpython")
-        if not hostpython:
-            # search in wanted_recipes if it's the first time
-            if "hostpython2" in ctx.wanted_recipes:
-                hostpython = "hostpython2"
-            elif "hostpython3" in ctx.wanted_recipes:
-                hostpython = "hostpython3"
-            else:
-                logger.error("No hostpython version set in the build.")
-                logger.error("Add python2 or python3 in your recipes:")
-                logger.error("./toolchain.py build python3 ...")
-                sys.exit(1)
-        if hostpython:
-            self.depends = [hostpython]
+        # The hostpython version was previously determined via
+        #     hostpython = ctx.state.get("hostpython")
+        hostpython = "hostpython3"
+        self.depends = [hostpython]
 
 
 recipe = HostpythonAliasRecipe()
--- recipes/python.py
+++ recipes/python.py
@@ -11,20 +11,10 @@ class PythonAliasRecipe(Recipe):
     is_alias = True
 
     def init_after_import(self, ctx):
-        python = ctx.state.get("python")
-        if not python:
-            # search in wanted_recipes if it's the first time
-            if "python2" in ctx.wanted_recipes:
-                python = "python2"
-            elif "python3" in ctx.wanted_recipes:
-                python = "python3"
-            else:
-                logger.error("No Python version set in the build.")
-                logger.error("Add python2 or python3 in your recipes:")
-                logger.error("./toolchain.py build python3 ...")
-                sys.exit(1)
-        if python:
-            self.depends = [python]
+        # The python version was previously deteremind via
+        #     python = ctx.state.get("python")
+        python = "python3"
+        self.depends = [python]
         self.recipe_dir = join(ctx.root_dir, "recipes", python)
 
 
