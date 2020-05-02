c06c51f149f1add9bf81d56ce7018749089de2f8
Andre Miras <andre.miras@gmail.com>
2020-05-02 22:37:11 +0200

    Uses new `cd` context manager more
    Also adds logging as we change directories.

--- recipes/pycrypto/__init__.py
+++ recipes/pycrypto/__init__.py
@@ -1,9 +1,8 @@
 '''Recipe for pycrypto on ios
 '''
-from toolchain import CythonRecipe, shprint
+from toolchain import CythonRecipe, shprint, cd
 from os.path import join
 import sh
-import os
 
 
 class PycryptoRecipe(CythonRecipe):
@@ -31,12 +30,12 @@ class PycryptoRecipe(CythonRecipe):
     def install(self):
         arch = list(self.filtered_archs)[0]
         build_dir = self.get_build_dir(arch.arch)
-        os.chdir(build_dir)
         hostpython = sh.Command(self.ctx.hostpython)
         build_env = arch.get_env()
         dest_dir = join(self.ctx.dist_dir, "root", "python")
         build_env['PYTHONPATH'] = join(dest_dir, 'lib', 'python2.7', 'site-packages')
-        shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+        with cd(build_dir):
+            shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
 
 
 recipe = PycryptoRecipe()
