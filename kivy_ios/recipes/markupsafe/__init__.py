da844fb50d6507ca5de6cdb946ae8abdac1e667f
richard <richard@dotmodus>
2020-05-25 22:27:25 +0200

    Remove markupsafe exclusion

--- kivy_ios/recipes/markupsafe/__init__.py
+++ kivy_ios/recipes/markupsafe/__init__.py
@@ -26,5 +26,4 @@ class MarkupSafeRecipe(PythonRecipe):
         shprint(cmd, "-i", "", "s/features\['speedups'\].*=.*speedups/pass/g", "./setup.py", _env=build_env)  # noqa: W605
         shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
 
-
 recipe = MarkupSafeRecipe()
