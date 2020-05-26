92fad1c66f2812ba365bfb12217b3a8e2088ab59
richard <richard@dotmodus>
2020-05-26 21:25:08 +0200

    :hammer: Fix jinja2 recipe

--- .ci/constants.py
+++ .ci/constants.py
@@ -6,9 +6,6 @@ BROKEN_RECIPES = set(
         # 'distutils.core' is not a package
         # https://github.com/kivy/kivy-ios/issues/467
         "markupsafe",
-        # depends on markupsafe
-        # https://github.com/kivy/kivy-ios/issues/466
-        "jinja2",
         # bad install directory or PYTHONPATH
         # https://github.com/kivy/kivy-ios/issues/468
         "werkzeug",
