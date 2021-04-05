8f80f8d7c97e9bffd17e5a7e363214536f8c1b09
Changbeom Ahn <kewlbear@gmail.com>
2021-04-05 13:48:59 +0900

    add bitcode support for SDL2

--- kivy_ios/recipes/sdl2_image/__init__.py
+++ kivy_ios/recipes/sdl2_image/__init__.py
@@ -15,6 +15,7 @@ class LibSDL2ImageRecipe(Recipe):
         shprint(sh.xcodebuild, self.ctx.concurrent_xcodebuild,
                 "ONLY_ACTIVE_ARCH=NO",
                 "ARCHS={}".format(arch.arch),
+                "BITCODE_GENERATION_MODE=bitcode",
                 "HEADER_SEARCH_PATHS={}".format(
                     join(self.ctx.include_dir, "common", "sdl2")),
                 "-sdk", arch.sdk,
