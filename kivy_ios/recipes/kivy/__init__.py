929897c84a4093a27a2beb9169b342842a73a065
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2022-04-22 21:50:36 -1000

    Bump Kivy recipe to master (#700)
    * Kivy with camera api enhancements
    
    * Update __init__.py

--- kivy_ios/recipes/kivy/__init__.py
+++ kivy_ios/recipes/kivy/__init__.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 class KivyRecipe(CythonRecipe):
-    version = "2.1.0"
+    version = "0e77e2ab94e54ebaf1f232f5dd66d4d103e08fbb"  # 2.1.0 + camera enhancements
     url = "https://github.com/kivy/kivy/archive/{version}.zip"
     library = "libkivy.a"
     depends = ["sdl2", "sdl2_image", "sdl2_mixer", "sdl2_ttf", "ios",
