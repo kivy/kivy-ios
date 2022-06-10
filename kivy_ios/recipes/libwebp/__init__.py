abf843db38f68c05a79741babdf1889c5bc80cad
Mathieu Virbel <mat@kivy.org>
2022-06-10 12:08:03 +0200

    Webp support in SDL2_image
    This patch allow to have the same support of webp in Kivy as Desktop and Android version.

---
+++ kivy_ios/recipes/libwebp/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from kivy_ios.toolchain import Recipe, shprint
+from os.path import join
+import sh
+
+class WebpRecipe(Recipe):
+    version = '1.0.3'
+    url = 'https://github.com/webmproject/libwebp/archive/v{version}.zip'
+
+    def build_arch(self, arch):
+        build_env = arch.get_env()
+        iosbuild = sh.Command(join(self.build_dir, "iosbuild.sh"))
+        shprint(iosbuild)
+
+recipe = WebpRecipe()
--- kivy_ios/recipes/sdl2_image/__init__.py
+++ kivy_ios/recipes/sdl2_image/__init__.py
@@ -4,20 +4,34 @@ import sh
 
 
 class LibSDL2ImageRecipe(Recipe):
-    version = "2.0.4"
+    version = "2.0.5"
     url = "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz"
     library = "Xcode-iOS/build/Release-{arch.sdk}/libSDL2_image.a"
     include_dir = "SDL_image.h"
-    depends = ["sdl2"]
+    depends = ["sdl2", "libwebp"]
     pbx_frameworks = ["CoreGraphics", "MobileCoreServices"]
 
+    def prebuild_arch(self, arch):
+        if self.has_marker("patched"):
+            return
+        self.apply_patch("webp.patch")
+        self.set_marker("patched")
+
     def build_arch(self, arch):
+        header_paths = [
+            "$HEADER_SEARCH_PATHS",
+            join(self.ctx.include_dir, "common", "sdl2"),
+        ]
+        framework_paths = [
+            "$FRAMEWORK_SEARCH_PATHS",
+            join(self.ctx.dist_dir, "frameworks"),
+        ]
         shprint(sh.xcodebuild, self.ctx.concurrent_xcodebuild,
                 "ONLY_ACTIVE_ARCH=NO",
                 "ARCHS={}".format(arch.arch),
                 "BITCODE_GENERATION_MODE=bitcode",
-                "HEADER_SEARCH_PATHS={}".format(
-                    join(self.ctx.include_dir, "common", "sdl2")),
+                "HEADER_SEARCH_PATHS={}".format(" ".join(header_paths)),
+                "FRAMEWORK_SEARCH_PATHS={}".format(" ".join(framework_paths)),
                 "-sdk", arch.sdk,
                 "-project", "Xcode-iOS/SDL_image.xcodeproj",
                 "-target", "libSDL_image-iOS",
