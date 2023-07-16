5f3ad37dc90bd5abfebf20f263d5fe0d6ba0efef
Mirko Galimberti <me@mirkogalimberti.com>
2023-07-16 15:52:13 +0200

    Update `libpng` recipe and build `SDL2_ttf` vendored `freetype` with png support, so can render colored emoji (#827)
    * Update libpng recipe and build SDL2_ttf vendored freetype with png support, so can render colored emoji
    
    * Revert to previous link, now that we can download from sourceforge (non-mirror)

--- kivy_ios/recipes/sdl2_ttf/__init__.py
+++ kivy_ios/recipes/sdl2_ttf/__init__.py
@@ -4,11 +4,11 @@ import sh
 
 
 class LibSDL2TTFRecipe(Recipe):
-    version = "2.20.1"
+    version = "2.20.2"
     url = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-{version}/SDL2_ttf-{version}.tar.gz"
     library = "Xcode/build/Release-{arch.sdk}/libSDL2_ttf.a"
     include_dir = "SDL_ttf.h"
-    depends = ["sdl2"]
+    depends = ["libpng", "sdl2"]
 
     def build_arch(self, arch):
         shprint(sh.xcodebuild, self.ctx.concurrent_xcodebuild,
@@ -16,8 +16,11 @@ class LibSDL2TTFRecipe(Recipe):
                 "ARCHS={}".format(arch.arch),
                 "BITCODE_GENERATION_MODE=bitcode",
                 "GENERATE_MASTER_OBJECT_FILE=YES",
-                "HEADER_SEARCH_PATHS={}".format(
-                    join(self.ctx.include_dir, "common", "sdl2")),
+                "HEADER_SEARCH_PATHS={sdl_include_dir} {libpng_include_dir}".format(
+                    sdl_include_dir=join(self.ctx.include_dir, "common", "sdl2"),
+                    libpng_include_dir=join(self.ctx.include_dir, "common", "libpng"),
+                ),
+                "GCC_PREPROCESSOR_DEFINITIONS=$(GCC_PREPROCESSOR_DEFINITIONS) FT_CONFIG_OPTION_USE_PNG=1",
                 "-sdk", arch.sdk,
                 "-project", "Xcode/SDL_ttf.xcodeproj",
                 "-target", "Static Library",
