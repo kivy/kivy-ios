8e3512172cd9b18033eec97b663e917b94a95b91
gdrosos <t8180024@aueb.gr>
2023-08-10 16:56:06 +0300

    Remove requests from KivyRecipe python_depends

--- kivy_ios/recipes/kivy/__init__.py
+++ kivy_ios/recipes/kivy/__init__.py
@@ -12,7 +12,7 @@ class KivyRecipe(CythonRecipe):
     library = "libkivy.a"
     depends = ["sdl2", "sdl2_image", "sdl2_mixer", "sdl2_ttf", "ios",
                "pyobjus", "python"]
-    python_depends = ["certifi", "charset-normalizer", "idna", "requests", "urllib3"]
+    python_depends = ["certifi", "charset-normalizer", "idna", "urllib3"]
     pbx_frameworks = ["OpenGLES", "Accelerate", "CoreMedia", "CoreVideo"]
     pre_build_ext = True
 
